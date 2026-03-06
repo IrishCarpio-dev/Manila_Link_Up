4d3928e3b4381315e60494e9027c8f89d0587ae9
Irish Carpio <IrishCarpio-dev@github.com>
2026-03-06 18:31:08 +0800

    getting started activity

---
+++ app/src/main/java/com/manilalinkup/app/GetStarted.java
@@ -0,0 +1,35 @@
+package com.manilalinkup.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class GetStarted extends AppCompatActivity {
+
+    CardView jobSeekerCard;
+    CardView employerCard;
+
+    MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_get_started);
+        jobSeekerCard = findViewById(R.id.card_view_job_seeker);
+        employerCard = findViewById(R.id.card_view_employer);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+}
--- app/src/main/java/com/manilalinkup/app/LoginActivity.java
+++ app/src/main/java/com/manilalinkup/app/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.manilalinkup.app;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
     MaterialToolbar toolbar;
+    MaterialButton loginNowButton;
+    TextInputEditText emailInput;
+    TextInputEditText passwordInput;
+    TextInputLayout emailLayout;
+    TextInputLayout passwordLayout;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -21,11 +33,50 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        loginNowButton = findViewById(R.id.material_button_login_now_2);
+        emailInput = findViewById(R.id.text_input_email_input);
+        passwordInput = findViewById(R.id.text_input_password_input);
+        emailLayout = findViewById(R.id.text_input_layout_email_address);
+        passwordLayout = findViewById(R.id.text_input_layout_password);
+
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        loginNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+                boolean isValid = true;
+
+                emailLayout.setError(null);
+                passwordLayout.setError(null);
+
+                if(email.isEmpty()){
+                    emailLayout.setError("Email is required.");
+                    emailInput.requestFocus();
+                    isValid = false;
+                } else if(password.isEmpty()){
+                    passwordLayout.setError("Password is required.");
+                    passwordInput.requestFocus();
+                    isValid = false;
+                } else if(password.length() < 8){
+                    passwordLayout.setError("Password must be at least 8 characters.");
+                    passwordInput.requestFocus();
+                    isValid = false;
+                } else{
+                    Intent loginNowActivityIntent = new Intent(LoginActivity.this, GetStarted.class);
+                    startActivity(loginNowActivityIntent);
+                    Toast.makeText(LoginActivity.this,"Logging you in. One moment... in time ;)", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
     }
 }
