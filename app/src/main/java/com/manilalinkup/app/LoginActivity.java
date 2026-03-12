2d2e4ac0904669057b831ec53ada27019d18ff0a
RVCaling <rvcaling11@gmail.com>
2026-03-12 16:13:43 +0800

    edit profile - seeker

--- app/src/main/java/com/manilalinkup/app/LoginActivity.java
+++ app/src/main/java/com/manilalinkup/app/LoginActivity.java
@@ -3,11 +3,14 @@ package com.manilalinkup.app;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
@@ -18,16 +21,11 @@ public class LoginActivity extends AppCompatActivity {
 
     MaterialToolbar toolbar;
     MaterialButton loginNowButton;
-
     TextInputEditText emailInput;
     TextInputEditText passwordInput;
-
     TextInputLayout emailLayout;
     TextInputLayout passwordLayout;
 
-    ImageView googleLoginButton;
-
-    private static final int RC_SIGN_IN = 100;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -35,45 +33,50 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-
-        // --- VIEW BINDING ---
         loginNowButton = findViewById(R.id.material_button_login_now_2);
         emailInput = findViewById(R.id.text_input_email_input);
         passwordInput = findViewById(R.id.text_input_password_input);
         emailLayout = findViewById(R.id.text_input_layout_email_address);
         passwordLayout = findViewById(R.id.text_input_layout_password);
-        googleLoginButton = findViewById(R.id.image_view_login_google);
-        toolbar = findViewById(R.id.toolbar);
 
+
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        loginNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String email = emailInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+                boolean isValid = true;
+
+                emailLayout.setError(null);
+                passwordLayout.setError(null);
+
+                if(email.isEmpty()){
+                    emailLayout.setError("Email is required.");
+                    emailInput.requestFocus();
+                    isValid = false;
+                } else if(password.isEmpty()){
+                    passwordLayout.setError("Password is required.");
+                    passwordInput.requestFocus();
+                    isValid = false;
+                } else if(password.length() < 8){
+                    passwordLayout.setError("Password must be at least 8 characters.");
+                    passwordInput.requestFocus();
+                    isValid = false;
+                } else{
+                    Intent loginNowActivityIntent = new Intent(LoginActivity.this, OTPVerificationActivity.class);
+                    startActivity(loginNowActivityIntent);
+                    Toast.makeText(LoginActivity.this,"Logging you in. One moment... in time ;)", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
-
-    private void loginUser() {
-        String email = emailInput.getText().toString().trim();
-        String password = passwordInput.getText().toString().trim();
-
-        emailLayout.setError(null);
-        passwordLayout.setError(null);
-
-        if (email.isEmpty()) {
-            emailLayout.setError("Email is required.");
-            emailInput.requestFocus();
-            return;
-        }
-
-        if (password.isEmpty()) {
-            passwordLayout.setError("Password is required.");
-            passwordInput.requestFocus();
-            return;
-        }
-
-        if (password.length() < 8) {
-            passwordLayout.setError("Password must be at least 8 characters.");
-            passwordInput.requestFocus();
-            return;
-        }
-    }
-
-    }
+}
