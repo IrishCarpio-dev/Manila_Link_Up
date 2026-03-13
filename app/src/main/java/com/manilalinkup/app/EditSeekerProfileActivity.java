2676250a7e93db2c1084d47df7ca53ab95b82414
Irish Carpio <IrishCarpio-dev@github.com>
2026-03-13 18:43:43 +0800

    get started

---
+++ app/src/main/java/com/manilalinkup/app/EditSeekerProfileActivity.java
@@ -0,0 +1,29 @@
+package com.manilalinkup.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class EditSeekerProfileActivity extends AppCompatActivity {
+
+    MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit_seeker_profile);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+}
