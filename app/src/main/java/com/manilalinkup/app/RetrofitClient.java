5f4fc4d8364125e59fa5fc899ab8a12649a80da5
IrishCarpio-dev <preciousirishc@gmail.com>
2026-04-02 11:47:46 +0800

    Refactors to RetrofitClient

--- app/src/main/java/com/manilalinkup/app/RetrofitClient.java
+++ app/src/main/java/com/manilalinkup/app/RetrofitClient.java
@@ -9,6 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     private static Retrofit retrofit = null;
 
+    // TODO: Change BASE_URL to actual server domain
     private static final String BASE_URL = "http://10.0.2.2:8000/api/";
 
     public static Retrofit getClient(String token) {
@@ -23,8 +24,7 @@ public class RetrofitClient {
                             .addHeader("Content-Type", "application/json")
                             .method(original.method(), original.body());
 
-                    // If a token exists (user is logged in), attach it
-                    if (token != null && !token.isEmpty()) {
+                    if (token != null && !token.isBlank()) {
                         requestBuilder.addHeader("Authorization", "Bearer " + token);
                     }
 
