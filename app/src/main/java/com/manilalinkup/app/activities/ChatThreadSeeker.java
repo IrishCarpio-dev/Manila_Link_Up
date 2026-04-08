ca0ce44a90056e34416ca2d133f0268ecf34b8f5
Irish Carpio <IrishCarpio-dev@github.com>
2026-04-09 01:37:39 +0800

    chat thread seeker creation

---
+++ app/src/main/java/com/manilalinkup/app/activities/ChatThreadSeeker.java
@@ -0,0 +1,20 @@
+package com.manilalinkup.app.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.manilalinkup.app.R;
+
+public class ChatThreadSeeker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chat_thread_seeker);
+
+
+    }
+}
