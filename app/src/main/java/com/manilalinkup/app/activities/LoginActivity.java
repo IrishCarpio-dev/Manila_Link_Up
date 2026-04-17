329751d8541cf07183390969af915500d55ed3c1
Irish Carpio <IrishCarpio-dev@github.com>
2026-04-17 22:00:49 +0800

    removes google and fb shortcut for user  dashboards

--- app/src/main/java/com/manilalinkup/app/activities/LoginActivity.java
+++ app/src/main/java/com/manilalinkup/app/activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.manilalinkup.app.activities;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -46,12 +45,6 @@ public class LoginActivity extends AppCompatActivity {
     TextInputLayout passwordLayout;
     TextView forgetPassword;
 
-    //for testing Dashboards - Irish
-    ImageView googleLogin;
-    ImageView facebookLogin; // Added for Facebook shortcut
-
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,31 +56,6 @@ public class LoginActivity extends AppCompatActivity {
         emailLayout = findViewById(R.id.text_input_layout_email_address);
         passwordLayout = findViewById(R.id.text_input_layout_password);
         forgetPassword = findViewById(R.id.text_view_forget_password);
-
-        // For testing Dashboards - Irish
-        googleLogin = findViewById(R.id.image_view_login_google);
-        googleLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent testIntents = new Intent(LoginActivity.this, EmployerDashboard.class);
-                startActivity(testIntents);
-            }
-        });
-
-        // For testing Seeker Dashboard via Facebook shortcut
-        facebookLogin = findViewById(R.id.image_view_login_facebook);
-        facebookLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Direct jump to Seeker Dashboard
-                Intent intent = new Intent(LoginActivity.this, SeekerDashboardActivity.class);
-                startActivity(intent);
-                finish(); // Optional: closes login screen so back button doesn't return here
-            }
-        });
-
-
-
 
         progressDialog = new android.app.ProgressDialog(this);
         progressDialog.setMessage("Logging in...");
