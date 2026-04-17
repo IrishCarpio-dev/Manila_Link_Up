e4f7a614656f5b176f9878552987a399c836130d
IrishCarpio-dev <preciousirishc@gmail.com>
2026-04-17 14:26:31 +0800

    Adds new fields to Seeker sign up

--- app/src/main/java/com/manilalinkup/app/models/SeekerRequest.java
+++ app/src/main/java/com/manilalinkup/app/models/SeekerRequest.java
@@ -3,7 +3,9 @@ package com.manilalinkup.app.models;
 public class SeekerRequest {
     String uid;
     String firstName;
+    String middleName;
     String lastName;
+    String suffix;
     String email;
     String address;
     String birthdate;
@@ -15,13 +17,15 @@ public class SeekerRequest {
     Boolean isOpenForWork;
     Boolean isVerified;
 
-    public SeekerRequest(String uid, String firstName, String lastName, String email,
-                         String address, String birthdate, String location, String mobileNumber,
+    public SeekerRequest(String uid, String firstName, String middleName, String lastName, String suffix,
+                         String email, String address, String birthdate, String location, String mobileNumber,
                          int salary, String profilePhotoUrl, String clearance_photo,
                          Boolean  isOpenForWork, Boolean isVerified) {
         this.uid = uid;
         this.firstName = firstName;
+        this.middleName = middleName.isEmpty() ? null : middleName;
         this.lastName = lastName;
+        this.suffix = suffix.isEmpty() ? null : suffix;
         this.email = email;
         this.address = address;
         this.birthdate = birthdate;
